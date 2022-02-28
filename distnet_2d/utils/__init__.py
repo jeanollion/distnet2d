ab5580f604b9a4b986c12ea78960b0e75d53c6ee
jeanollion <smogollion@yahoo.fr>
2022-02-28 19:33:49 +0100

    fixed import

--- distnet_2d/utils/__init__.py
+++ distnet_2d/utils/__init__.py
@@ -1,3 +1,4 @@
 name="utils"
 from .callbacks import PatchedModelCheckpoint, PersistentReduceLROnPlateau
-from .helpers import predict_average_flip_rotate, edm_contour_loss, weighted_loss_by_category
+from .helpers import predict_average_flip_rotate
+from .losses import edm_contour_loss, weighted_loss_by_category
