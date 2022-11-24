fixed weighted gradient layer

distnet_2d/model/layers.py
@@ -342,8 +342,11 @@ class WeigthedGradient(tf.keras.layers.Layer):
 
     @tf.custom_gradient
     def op(self, x):
-        def grad(dy):
-            return dy * self.weight
+        def grad(*dy):
+            if isinstance(dy, tuple) and len(dy)>1:
+                return (y * self.weight for y in dy)
+            else:
+                return dy * self.weight
         return x, grad
 
 ############### MOBILE NET LAYERS ############################################################