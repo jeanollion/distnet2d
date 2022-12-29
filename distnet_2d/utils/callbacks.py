ea51fd6e436a46a5a72c556efae0482a8df6a53f
jeanollion <smogollion@yahoo.fr>
2022-12-29 11:08:51 +0100

    added stop on LR callback

--- distnet_2d/utils/callbacks.py
+++ distnet_2d/utils/callbacks.py
@@ -3,6 +3,17 @@ import warnings
 from time import sleep
 import subprocess
 from tensorflow.keras.callbacks import Callback, ReduceLROnPlateau
+from tensorflow.keras import backend
+
+class StopOnLR(Callback):
+    def __init__( self, min_lr, **kwargs, ):
+        super().__init__()
+        self.min_lr = min_lr
+
+	def on_epoch_start(self, epoch, logs={}):
+        lr = backend.get_value(self.model.optimizer.lr)
+		if(lr <= self.min_lr):
+			self.model.stop_training = True
 
 class PatchedModelCheckpoint(Callback):
     """Save the model after every epoch.
