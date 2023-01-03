c7bb992d9b3cb013a2043591ae16048895d173e2
jeanollion <smogollion@yahoo.fr>
2023-01-03 09:42:18 +0100

    minor modification on StopOnLR

--- distnet_2d/utils/callbacks.py
+++ distnet_2d/utils/callbacks.py
@@ -10,7 +10,7 @@ class StopOnLR(Callback):
         super().__init__()
         self.min_lr = min_lr
 
-    def on_epoch_start(self, epoch, logs={}):
+    def on_epoch_end(self, epoch, logs={}):
         lr = backend.get_value(self.model.optimizer.lr)
         if(lr <= self.min_lr):
             self.model.stop_training = True
