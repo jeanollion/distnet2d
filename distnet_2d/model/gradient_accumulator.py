- fixed lovasz loss per label - motion_loss: each gradient can be scaled - grad accumulator: MEAN reduction (was SUM)

distnet_2d/model/gradient_accumulator.py
@@ -10,6 +10,7 @@ class GradientAccumulator():
                                               )
         self.first_call = True
         self.gradient_accumulation = None
+        self.scale = tf.cast(1./accum_steps, tf.float32)
         self.reinit_grad_accum()
 
     def apply_accu_gradients(self):
@@ -32,9 +33,11 @@ class GradientAccumulator():
             ) for i, v in enumerate(self.model.trainable_variables)
         ]
 
-    def accumulate_gradients(self, gradients):
+    def accumulate_gradients(self, gradients, scale=True):
         for i in range(len(self.gradient_accumulation)):
             if gradients[i] is not None:
+                if scale:
+                    gradient[i] = self.scale * gradient[i]
                 self.gradient_accumulation[i].assign_add(gradients[i], read_value=False)
 
     def apply_gradients(self):

distnet_2d/utils/lovasz_loss.py
@@ -39,26 +39,39 @@ def lovasz_hinge(logits, labels, per_image=True, ignore=None, per_object=False, 
         elif channel_axis:
             trans = lambda x, shape: tf.reshape(tf.transpose(x, perm=[0, 3, 1, 2]), tf.concat([[-1], shape[1:3]], 0))
             utrans = lambda x, shape: tf.reshape(x, tf.concat([shape[:1], shape[-1:]], 0))
+
         def treat_image(log_lab):
             log, lab = log_lab
             log, lab = tf.expand_dims(log, 0), tf.expand_dims(lab, 0)
             log, lab = flatten_binary_scores(log, lab, ignore)
             return lovasz_hinge_flat(log, lab)
 
+        @tf.function
         def treat_image_per_label(log_lab):
             log, lab = log_lab
             log, lab = tf.expand_dims(log, 0), tf.expand_dims(lab, 0)
             log, lab = flatten_binary_scores(log, lab, ignore)
             unique_labs, _ = tf.unique(lab)
-            unique_labs = tf.boolean_mask(unique_labs, tf.math.greater(unique_labs, 0))
-            def treat_label(label):
-                valid = tf.equal(lab, label)
-                vscores = tf.boolean_mask(log, valid)
-                vlabels = tf.boolean_mask(lab, valid)
-                return lovasz_hinge_flat(vscores, vlabels)
-            loss_per_label = tf.cond(tf.equal(tf.shape(unique_labs)[0], 0),
-                           lambda: tf.cast(0, tf.float32),
-                           lambda: tf.map_fn(treat_label, unique_labs, fn_output_signature=tf.float32) )
+            accum = 0.
+            for label in unique_labs:
+                if label>0:
+                    valid = tf.equal(lab, label)
+                    vscores = tf.boolean_mask(log, valid)
+                    vlabels = tf.boolean_mask(lab, valid)
+                    accum = accum + lovasz_hinge_flat(vscores, vlabels)
+            if tf.shape(unique_labs)[0]>1:
+                accum = accum / tf.cast(tf.shape(unique_labs)[0]-1, tf.float32)
+            return accum
+            #unique_labs = tf.boolean_mask(unique_labs, tf.math.greater(unique_labs, 0))
+            # unique_labs = unique_labs[unique_labs != 0]
+            # def treat_label(label):
+            #     valid = tf.equal(lab, label)
+            #     vscores = tf.boolean_mask(log, valid)
+            #     vlabels = tf.boolean_mask(lab, valid)
+            #     return lovasz_hinge_flat(vscores, vlabels)
+            # loss_per_label = tf.cond(tf.equal(tf.shape(unique_labs)[0], 0),
+            #                lambda: tf.convert_to_tensor([0.], tf.float32),
+            #                lambda: tf.map_fn(treat_label, unique_labs, fn_output_signature=tf.float32) )
             return tf.reduce_mean(loss_per_label)
         shape = tf.shape(labels)
         if per_object or channel_axis:

distnet_2d/utils/objectwise_motion_losses.py
@@ -2,16 +2,21 @@ import tensorflow as tf
 from .coordinate_op_2d import get_weighted_mean_2d_fun, get_center_distance_spread_fun
 from ..model.layers import WeigthedGradient
 
-def get_motion_losses(spatial_dims, motion_range:int, grad_weight_ratio:float, center_motion:bool = True, motion_var:bool=True):
+def get_motion_losses(spatial_dims, motion_range:int, center_displacement_grad_weight_center:float, center_displacement_grad_weight_displacement:float, center_motion:bool = True, motion_var:bool=True):
     nan = tf.cast(float('NaN'), tf.float32)
     #motion_loss_fun = _center_spread_loss(spatial_dims)
     motion_loss_fun = _distance_loss()
     center_fun = get_weighted_mean_2d_fun(spatial_dims, True, batch_axis = False, keepdims = False)
     @tf.custom_gradient
-    def wgrad(x):
+    def wgrad_c(x):
         def grad(dy):
-            #print(f"grad: {dy}")
-            return dy * grad_weight_ratio
+            return dy * center_displacement_grad_weight_center
+        return x, grad
+    @tf.custom_gradient
+    def wgrad_d(x):
+        def grad(dy):
+            #print(f"grad displacement: {dy}")
+            return dy * center_displacement_grad_weight_displacement
         return x, grad
     def fun(args):
         dY, dX, center, labels, prev_labels = args
@@ -21,10 +26,11 @@ def get_motion_losses(spatial_dims, motion_range:int, grad_weight_ratio:float, c
         if center_motion: # center+motion coherence loss
             # predicted  center coord per object
             dm = tf.stack([dYm, dXm], -1) # (T-1, N, 2)
-            dm=wgrad(dm)
+            dm=wgrad_d(dm)
             center_values = tf.math.exp(-center)
             #center_values = tf.math.exp(-tf.math.square(center)) # numerical instability on gradients
             center_ob = center_fun(tf.math.multiply_no_nan(tf.expand_dims(center_values, -1), label_rank))
+            center_ob = wgrad_c(center_ob)
             prev_labels = prev_labels[...,:N] # (T-1, N) # trim from (T-1, Nmax)
             has_prev = tf.math.greater(prev_labels, 0)
             prev_idx = tf.cast(has_prev, tf.int32) * (prev_labels-1)