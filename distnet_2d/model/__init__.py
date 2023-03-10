34d472450bdb79ea7a7f0cd0429b93aee7920830
jeanollion <smogollion@yahoo.fr>
2023-03-10 12:06:36 +0100

    fixed error introduced in last commit

--- distnet_2d/model/__init__.py
+++ distnet_2d/model/__init__.py
@@ -1,4 +1,4 @@
 name="models"
 from .attention import SpatialAttention2D
 from .layers import ConstantConvolution2D, ReflectionPadding2D
-from .distnet_2d import get_distnet_2d_sep_out_fw
+from .distnet_2d import get_distnet_2d
