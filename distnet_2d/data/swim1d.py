972162a9392f29179962a7cd1230905583db4d97
jeanollion <smogollion@yahoo.fr>
2023-11-09 13:16:50 +0100

    minor changes

--- distnet_2d/data/swim1d.py
+++ distnet_2d/data/swim1d.py
@@ -20,12 +20,9 @@ def get_swim1d_function(mask_channels:list, distance:int=50, min_gap:int=3, clos
                 space_y = [slice_obj[0] for slice_obj in space_y] # only first dim
                 limit = mask_img.shape[0]
                 space_y = [slice_obj for slice_obj in space_y if (slice_obj.stop - slice_obj.start)>=min_gap and slice_obj.stop>15 and (closed_end or slice_obj.start>0) and slice_obj.stop<limit] # keep only slices with length > 4 and not the space close to the open ends
-                #space_y = [slice_obj for slice_obj in space_y if (slice_obj.stop - slice_obj.start)>2 and (self.closed_end or slice_obj.start>0) and slice_obj.stop<limit] # keep only slices with length > 2 and not the space close to the open ends
                 if len(space_y)>0:
                     space_y = [(slice_obj.stop + slice_obj.start)//2 for slice_obj in space_y]
-                    #space_y_mean = [(slice_obj.stop + slice_obj.start)//2 if slice_obj.start>0 else 0 for slice_obj in space_y]
                     y = choice(space_y)
-                    #y = choice(space_y_mean)
                     lower = closed_end or not getrandbits(1)
                     if y==0:
                         lower = True
