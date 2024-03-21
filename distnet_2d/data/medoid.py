963c0040ca1c2201650b855327b9ac04c5d0a77d
jeanollion <smogollion@yahoo.fr>
2024-03-21 20:29:59 +0100

    - improved medoid computation speed

--- distnet_2d/data/medoid.py
+++ distnet_2d/data/medoid.py
@@ -1,19 +1,18 @@
 from numba import jit
 import numpy as np
-# Adapted from Original work Copyright 2021 Manan Lalit, Max Planck Institute of Molecular Cell Biology and Genetics  (MIT License https://github.com/juglab/EmbedSeg/blob/main/LICENSE)
 
 def get_medoid(Y, X):
-    dist_matrix = pairwise_python(Y, X)
-    imin = np.argmin(np.sum(dist_matrix, axis=0))
+    dist_array = distarray_python(Y, X)
+    imin = np.argmin(dist_array)
     return Y[imin], X[imin]
 
 @jit(nopython=True)
-def pairwise_python(Y, X):
+def distarray_python(Y, X):
     N = Y.shape[0]
-    D = np.empty((N, N), dtype=np.float32) # np.empty -> faster than np.zeros
+    D = np.zeros((N,), dtype=np.float32)
     for i in range(0, N-1):
         for j in range(i+1, N):
-            D[j, i] = D[i, j] = np.sqrt( (Y[i] - Y[j])**2 + (X[i] - X[j])**2 )
-    for i in range(N): # diagonal is always zero but must be filled as np.empty is used
-        D[i, i] = 0.0
+            d = np.sqrt( (Y[i] - Y[j])**2 + (X[i] - X[j])**2 )
+            D[j] += d
+            D[i] += d
     return D
