8373de0b4bca49a7d81b9c1cd92b9944715d0474
jeanollion <smogollion@yahoo.fr>
2024-05-22 22:13:13 +0200

    moved callbacks to dataset_iterator

--- distnet_2d/utils/__init__.py
+++ distnet_2d/utils/__init__.py
@@ -1,4 +1,3 @@
 name="utils"
-from .callbacks import StopOnLR, EpsilonCosineDecayCallback, LogsCallback, SafeModelCheckpoint, ReduceLROnPlateau2
 from .helpers import predict_average_flip_rotate
 from .losses import PseudoHuber, weighted_loss_by_category, balanced_category_loss
