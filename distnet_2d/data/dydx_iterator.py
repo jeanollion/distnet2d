- option to predict edm and gcdm derivatives - fixed gcdm computation at interface between cells

distnet_2d/data/dydx_iterator.py
@@ -12,6 +12,7 @@ import itertools
 import edt
 from random import random
 from .medoid import get_medoid
+import time
 
 class DyDxIterator(TrackingIterator):
     def __init__(self,
@@ -217,9 +218,13 @@ class DyDxIterator(TrackingIterator):
             if return_next:
                 for j in range(0, frame_window):
                     self._erase_small_objects_at_edges(labelIms[i,...,frame_window+1+j], i, mask_to_erase_next, [m+j for m in mask_to_erase_chan_next], batch_by_channel)
+        object_slices = {}
+        for b, c in itertools.product(range(labelIms.shape[0]), range(labelIms.shape[-1])):
+            object_slices[(b, c)] = find_objects(labelIms[b,...,c])
         edm = np.zeros(shape=labelIms.shape, dtype=np.float32)
         for b,c in itertools.product(range(edm.shape[0]), range(edm.shape[-1])):
             edm[b,...,c] = edt.edt(labelIms[b,...,c], black_border=False)
+            #_compute_edm(edm[b,...,c], labelIms[b,...,c], object_slices[(b, c)])
         n_motion = 2 * frame_window if return_next else frame_window
         if long_term:
             n_motion = n_motion + (2 * ( frame_window - 1 ) if return_next else frame_window -1)
@@ -255,23 +260,27 @@ class DyDxIterator(TrackingIterator):
             for c in range(0, frame_window):
                 sel = [c, c+1]
                 l_c = [labels_and_centers[(i,s)] for s in sel]
-                _compute_displacement(l_c, labelIms[i][...,sel], labels_map_prev[bidx][c], dyIm[i,...,c], dxIm[i,...,c], dyImNext=dyImNext[i,...,c] if ndisp else None, dxImNext=dxImNext[i,...,c] if ndisp else None, gdcmIm=centerIm[i,...,frame_window] if self.return_center and sel[1] == frame_window else None, gdcmImPrev=centerIm[i,...,c] if self.return_center else None, linkMultiplicityIm=linkMultiplicityIm[i,...,c] if self.return_link_multiplicity else None, linkMultiplicityImNext=linkMultiplicityImNext[i,...,c] if self.return_link_multiplicity and ndisp else None, rankIm=labelIm[i,...,frame_window] if self.return_label_rank and sel[1] == frame_window else None, rankImPrev=labelIm[i,...,c] if self.return_label_rank else None, prevLabelArr=prevLabelArr[i,c] if self.return_label_rank else None, nextLabelArr=nextLabelArr[i,c] if self.return_label_rank and ndisp else None, centerArr=centerArr[i,frame_window] if self.return_label_rank and sel[1] == frame_window else None, centerArrPrev=centerArr[i,c] if self.return_label_rank else None, center_mode=self.center_mode)
+                o_s = [object_slices[(i, s)] for s in sel]
+                _compute_displacement(l_c, labelIms[i][...,sel], labels_map_prev[bidx][c], o_s, dyIm[i,...,c], dxIm[i,...,c], dyImNext=dyImNext[i,...,c] if ndisp else None, dxImNext=dxImNext[i,...,c] if ndisp else None, gdcmIm=centerIm[i,...,frame_window] if self.return_center and sel[1] == frame_window else None, gdcmImPrev=centerIm[i,...,c] if self.return_center else None, linkMultiplicityIm=linkMultiplicityIm[i,...,c] if self.return_link_multiplicity else None, linkMultiplicityImNext=linkMultiplicityImNext[i,...,c] if self.return_link_multiplicity and ndisp else None, rankIm=labelIm[i,...,frame_window] if self.return_label_rank and sel[1] == frame_window else None, rankImPrev=labelIm[i,...,c] if self.return_label_rank else None, prevLabelArr=prevLabelArr[i,c] if self.return_label_rank else None, nextLabelArr=nextLabelArr[i,c] if self.return_label_rank and ndisp else None, centerArr=centerArr[i,frame_window] if self.return_label_rank and sel[1] == frame_window else None, centerArrPrev=centerArr[i,c] if self.return_label_rank else None, center_mode=self.center_mode)
             if return_next:
                 for c in range(frame_window, 2*frame_window):
                     sel = [c, c+1]
                     l_c = [labels_and_centers[(i, s)] for s in sel]
-                    _compute_displacement(l_c, labelIms[i][...,sel], labels_map_prev[bidx][c], dyIm[i,...,c], dxIm[i,...,c], dyImNext=dyImNext[i,...,c] if ndisp else None, dxImNext=dxImNext[i,...,c] if ndisp else None, gdcmIm=centerIm[i,..., c + 1] if self.return_center else None, gdcmImPrev=None, linkMultiplicityIm=linkMultiplicityIm[i,...,c] if self.return_link_multiplicity else None, linkMultiplicityImNext=linkMultiplicityImNext[i,...,c] if self.return_link_multiplicity and ndisp else None, rankIm=labelIm[i,..., c + 1] if self.return_label_rank else None, rankImPrev=None, prevLabelArr=prevLabelArr[i,c] if self.return_label_rank else None, nextLabelArr=nextLabelArr[i,c] if self.return_label_rank and ndisp else None, centerArr=centerArr[i, c + 1] if self.return_label_rank else None, center_mode=self.center_mode)
+                    o_s = [object_slices[(i, s)] for s in sel]
+                    _compute_displacement(l_c, labelIms[i][...,sel], labels_map_prev[bidx][c], o_s, dyIm[i,...,c], dxIm[i,...,c], dyImNext=dyImNext[i,...,c] if ndisp else None, dxImNext=dxImNext[i,...,c] if ndisp else None, gdcmIm=centerIm[i,..., c + 1] if self.return_center else None, gdcmImPrev=None, linkMultiplicityIm=linkMultiplicityIm[i,...,c] if self.return_link_multiplicity else None, linkMultiplicityImNext=linkMultiplicityImNext[i,...,c] if self.return_link_multiplicity and ndisp else None, rankIm=labelIm[i,..., c + 1] if self.return_label_rank else None, rankImPrev=None, prevLabelArr=prevLabelArr[i,c] if self.return_label_rank else None, nextLabelArr=nextLabelArr[i,c] if self.return_label_rank and ndisp else None, centerArr=centerArr[i, c + 1] if self.return_label_rank else None, center_mode=self.center_mode)
             if long_term:
                 off = 2*frame_window if return_next else frame_window
                 for c in range(0, frame_window-1):
                     sel = [c, frame_window]
                     l_c = [labels_and_centers[(i, s)] for s in sel]
-                    _compute_displacement(l_c, labelIms[i][...,sel], labels_map_prev[bidx][c+off], dyIm[i,...,c+off], dxIm[i,...,c+off], dyImNext=dyImNext[i,...,c+off] if ndisp else None, dxImNext=dxImNext[i,...,c+off] if ndisp else None, gdcmIm=None, gdcmImPrev=None, linkMultiplicityIm=linkMultiplicityIm[i,..., c + off] if self.return_link_multiplicity else None, linkMultiplicityImNext=linkMultiplicityImNext[i,..., c + off] if self.return_link_multiplicity and ndisp else None, rankIm=None, rankImPrev=None, prevLabelArr=prevLabelArr[i, c + off] if self.return_label_rank else None, nextLabelArr=nextLabelArr[i, c + off] if self.return_label_rank and ndisp else None, center_mode=self.center_mode)
+                    o_s = [object_slices[(i, s)] for s in sel]
+                    _compute_displacement(l_c, labelIms[i][...,sel], labels_map_prev[bidx][c+off], o_s, dyIm[i,...,c+off], dxIm[i,...,c+off], dyImNext=dyImNext[i,...,c+off] if ndisp else None, dxImNext=dxImNext[i,...,c+off] if ndisp else None, gdcmIm=None, gdcmImPrev=None, linkMultiplicityIm=linkMultiplicityIm[i,..., c + off] if self.return_link_multiplicity else None, linkMultiplicityImNext=linkMultiplicityImNext[i,..., c + off] if self.return_link_multiplicity and ndisp else None, rankIm=None, rankImPrev=None, prevLabelArr=prevLabelArr[i, c + off] if self.return_label_rank else None, nextLabelArr=nextLabelArr[i, c + off] if self.return_label_rank and ndisp else None, center_mode=self.center_mode)
                 if return_next:
                     for c in range(frame_window-1, 2*(frame_window-1)):
                         sel = [frame_window, c+3]
                         l_c = [labels_and_centers[(i, s)] for s in sel]
-                        _compute_displacement(l_c, labelIms[i][...,sel], labels_map_prev[bidx][c+off], dyIm[i,...,c+off], dxIm[i,...,c+off], dyImNext=dyImNext[i,...,c+off] if ndisp else None, dxImNext=dxImNext[i,...,c+off] if ndisp else None, gdcmIm=None, gdcmImPrev=None, linkMultiplicityIm=linkMultiplicityIm[i,..., c + off] if self.return_link_multiplicity else None, linkMultiplicityImNext=linkMultiplicityImNext[i,..., c + off] if self.return_link_multiplicity and ndisp else None, rankIm=None, rankImPrev=None, prevLabelArr=prevLabelArr[i, c + off] if self.return_label_rank else None, nextLabelArr=nextLabelArr[i, c + off] if self.return_label_rank and ndisp else None, center_mode=self.center_mode)
+                        o_s = [object_slices[(i, s)] for s in sel]
+                        _compute_displacement(l_c, labelIms[i][...,sel], labels_map_prev[bidx][c+off], o_s, dyIm[i,...,c+off], dxIm[i,...,c+off], dyImNext=dyImNext[i,...,c+off] if ndisp else None, dxImNext=dxImNext[i,...,c+off] if ndisp else None, gdcmIm=None, gdcmImPrev=None, linkMultiplicityIm=linkMultiplicityIm[i,..., c + off] if self.return_link_multiplicity else None, linkMultiplicityImNext=linkMultiplicityImNext[i,..., c + off] if self.return_link_multiplicity and ndisp else None, rankIm=None, rankImPrev=None, prevLabelArr=prevLabelArr[i, c + off] if self.return_label_rank else None, nextLabelArr=nextLabelArr[i, c + off] if self.return_label_rank and ndisp else None, center_mode=self.center_mode)
         other_output_channels = [chan_idx for chan_idx in self.output_channels if chan_idx!=1 and chan_idx!=2]
         all_channels = [batch_by_channel[chan_idx] for chan_idx in other_output_channels]
         channel_inc = 0
@@ -371,7 +380,7 @@ def _get_labels_and_centers(labelIm, edm, center_mode = "GEOMETRICAL"):
         assert edm is not None and edm.shape == labelIm.shape
         centers = []
         for label in labels:
-            edm_label = ma.array(edm, mask = labelIm != label)
+            edm_label = ma.array(edm, mask=labelIm != label)
             center = ma.argmax(edm_label, fill_value=0)
             center = np.unravel_index(center, edm_label.shape)
             centers.append(center)
@@ -381,7 +390,7 @@ def _get_labels_and_centers(labelIm, edm, center_mode = "GEOMETRICAL"):
     elif center_mode == "SKELETON":
         assert edm is not None and edm.shape == labelIm.shape
         mass_centers = np.array(center_of_mass(labelIm, labelIm, labels))[np.newaxis] # 1, N_ob, 2
-        lm_coords = peak_local_max(edm, labels = labelIm) # N_lm, 2
+        lm_coords = peak_local_max(edm, labels=labelIm) # N_lm, 2
         lm_coords_l = labelIm[lm_coords[:,0], lm_coords[:,1]] # N_lm
         # labels in labelIm are not necessarily continuous -> replace by rank
         label_rank = np.zeros(shape=(max(labels)+1,), dtype=np.int32)
@@ -480,7 +489,7 @@ def _get_category(n_neigh):
     else:
         return 2
 
-def _compute_displacement(labels_map_centers, labelIm, labels_map_prev, dyIm, dxIm, dyImNext=None, dxImNext=None, gdcmIm=None, gdcmImPrev=None, linkMultiplicityIm=None, linkMultiplicityImNext=None, rankIm=None, rankImPrev=None, prevLabelArr=None, nextLabelArr=None, centerArr=None, centerArrPrev=None, center_mode:str= "MEDOID"):
+def _compute_displacement(labels_map_centers, labelIm, labels_map_prev, object_slices, dyIm, dxIm, dyImNext=None, dxImNext=None, gdcmIm=None, gdcmImPrev=None, linkMultiplicityIm=None, linkMultiplicityImNext=None, rankIm=None, rankImPrev=None, prevLabelArr=None, nextLabelArr=None, centerArr=None, centerArrPrev=None, center_mode:str= "MEDOID"):
     assert labelIm.shape[-1] == 2, f"invalid labelIm : {labelIm.shape[-1]} channels instead of 2"
     assert (dxImNext is None) == (dyImNext is None)
     if len(labels_map_centers[-1])==0: # no cells
@@ -526,10 +535,10 @@ def _compute_displacement(labels_map_centers, labelIm, labels_map_prev, dyIm, dx
                 rankImPrev[mask] = rank + 1
     if gdcmIm is not None:
         assert gdcmIm.shape == dyIm.shape, "invalid shape for center image"
-        _draw_centers(gdcmIm, labels_map_centers[-1], labelIm[...,1], geometrical_distance=center_mode == "GEOMETRICAL")
+        _draw_centers(gdcmIm, labels_map_centers[-1], labelIm[...,1], object_slices[1], geometrical_distance=center_mode == "GEOMETRICAL")
     if gdcmImPrev is not None:
         assert gdcmImPrev.shape == dyIm.shape, "invalid shape for center image prev"
-        _draw_centers(gdcmImPrev, labels_map_centers[0], labelIm[...,0], geometrical_distance=center_mode == "GEOMETRICAL")
+        _draw_centers(gdcmImPrev, labels_map_centers[0], labelIm[...,0], object_slices[0], geometrical_distance=center_mode == "GEOMETRICAL")
     if centerArr is not None:
         for rank, (label, center) in enumerate(labels_map_centers[-1].items()):
             centerArr[rank,0] = center[0]
@@ -539,20 +548,27 @@ def _compute_displacement(labels_map_centers, labelIm, labels_map_prev, dyIm, dx
             centerArrPrev[rank,0] = center[0]
             centerArrPrev[rank,1] = center[1]
 
-def _draw_centers(centerIm, labels_map_centers, labelIm, geometrical_distance:bool=False):
+def _draw_centers(centerIm, labels_map_centers, labelIm, object_slices, geometrical_distance:bool=False):
     if len(labels_map_centers)==0:
         return
     # geodesic distance to center
     if not geometrical_distance:
-        count = 0
-        m = np.ones_like(labelIm)
-        for center in labels_map_centers.values():
-            if not (isnan(center[0]) or isnan(center[1])):
-                m[int(round(center[0])), int(round(center[1]))] = 0
-                count+=1
-        if count>0:
-            m = ma.masked_array(m, ~labelIm.astype(bool))
-            centerIm[:] = skfmm.distance(m)
+        shape = centerIm.shape
+        labelIm_dil = maximum_filter(labelIm, size=5)
+        non_zero = labelIm>0
+        labelIm_dil[non_zero] = labelIm[non_zero]
+        for (i, sl) in enumerate(object_slices):
+            if sl is not None:
+                center = labels_map_centers.get(i+1)
+                if not (isnan(center[0]) or isnan(center[1])):
+                    sl = tuple( [slice(max(0, s.start - 2), min(s.stop + 2, ax - 1), s.step) for s, ax in zip(sl, shape)])
+                    mask = labelIm_dil == i + 1
+                    sub_m = mask[sl]
+                    m = np.ones_like(sub_m)
+                    #print(f"label: {i+1} slice: {sl}, center: {center}, sub_m {sub_m.shape}, coord: {(int(round(center[0]))-sl[0].start, int(round(center[1]))-sl[1].start)}", flush=True)
+                    m[int(round(center[0]))-sl[0].start, int(round(center[1]))-sl[1].start] = 0
+                    m = ma.masked_array(m, ~sub_m)
+                    centerIm[sl][sub_m] = skfmm.distance(m)[sub_m]
     else:
         Y, X = centerIm.shape
         Y, X = np.meshgrid(np.arange(Y, dtype = np.float32), np.arange(X, dtype = np.float32), indexing = 'ij')
@@ -565,3 +581,17 @@ def _draw_centers(centerIm, labels_map_centers, labelIm, geometrical_distance:bo
                 if mask.sum()>0:
                     d = np.sqrt(np.square(Y-center[0])+np.square(X-center[1]))
                     centerIm[mask] = d[mask]
+
+def _compute_edm(edmIm, labelIm, object_slices):
+    shape = edmIm.shape
+    for (i, sl) in enumerate(object_slices):
+        if sl is not None:
+            sl = tuple([slice(s.start - 1 if s.start>0 else 0, s.stop+1 if s.stop<ax-1 else s.stop, s.step) for s, ax in zip(sl, shape)])
+            mask = labelIm == i+1
+            sub_m = mask[sl]
+            sub_edm = np.copy(edmIm[sl])
+            sub_edm[np.logical_not(sub_m)] = 0  # remove neighbor cells
+            m = np.zeros_like(sub_m)
+            m[sub_m] = 1
+            edmIm[sl][sub_m] = skfmm.distance(m)[sub_m]
+

distnet_2d/model/distnet_2d.py
@@ -24,6 +24,7 @@ class DiSTNetModel(tf.keras.Model):
                  frame_window=3,
                  long_term:bool=True,
                  predict_next_displacement:bool=True,
+                 predict_gcdm_derivatives:bool=False, predict_edm_derivatives:bool=False,
                  print_gradients:bool=False,  # for optimization, available in eager mode only
                  accum_steps=1, use_agc=False, agc_clip_factor=0.1, agc_eps=1e-3, agc_exclude_output=False,  # lower clip factor clips more
                  **kwargs):
@@ -38,10 +39,11 @@ class DiSTNetModel(tf.keras.Model):
         self.frame_window = frame_window
         self.edm_loss = edm_loss
         self.edm_derivatives = edm_derivatives
-        self.gdcm_loss = gcdm_loss
+        self.gcdm_loss = gcdm_loss
         self.gcdm_derivatives = gcdm_derivatives
         self.displacement_loss = displacement_loss
-
+        self.predict_gcdm_derivatives = predict_gcdm_derivatives
+        self.predict_edm_derivatives = predict_edm_derivatives
         min_class_frequency=category_class_frequency_range[0]
         max_class_frequency=category_class_frequency_range[1]
         if category_weights is not None:
@@ -58,7 +60,7 @@ class DiSTNetModel(tf.keras.Model):
         self.use_agc = use_agc
         self.agc_clip_factor = agc_clip_factor
         self.agc_eps = agc_eps
-        self.agc_exclude_keywords=["DecoderTrackY0/, DecoderTrackX0/", "DecoderCat0/", "DecoderCenter0/", "DecoderSegEDM0"] if agc_exclude_output else None
+        self.agc_exclude_keywords=["DecoderTrackY0/, DecoderTrackX0/", "DecoderCat0/", "DecoderCenterGCDM0/", "DecoderCenterGCDMdY0/", "DecoderCenterGCDMdX0/", "DecoderSegEDM0/", "DecoderSegEDMdY0/", "DecoderSegEDMdX0/"] if agc_exclude_output else None
         self.print_gradients=print_gradients
 
     def train_step(self, data):
@@ -80,25 +82,35 @@ class DiSTNetModel(tf.keras.Model):
         if category_weight>0:
             assert len(y) == 5 , f"invalid number of output. Expected: >=4 actual {len(y)}" # 0 = edm, 1 = center, 2 = dY, 3 = dX, 4 = cat
         else:
-            assert len(y) >= 4, f"invalid number of output. Expected: >=4 actual {len(y)}"  # 0 = edm, 1 = center, 2 = dY, 3 = dX, 4 = cat
+            assert len(y) >= 4, f"invalid number of output. Expected: >=4 actual {len(y)}"  # 0 = edm, 1 = center, 2 = dY, 3 = dX
 
         with tf.GradientTape(persistent=self.print_gradients) as tape:
             y_pred = self(x, training=True)  # Forward pass
+            if self.predict_edm_derivatives:
+                edm, edm_dy, edm_dx = tf.split(y_pred[0], num_or_size_splits=3, axis=-1)
+            else:
+                edm, edm_dy, edm_dx = y_pred[0], None, None
+            if self.predict_gcdm_derivatives:
+                gcdm, gcdm_dy, gcdm_dx = tf.split(y_pred[1], num_or_size_splits=3, axis=-1)
+            else:
+                gcdm, gcdm_dy, gcdm_dx = y_pred[1], None, None
+
             # compute loss
             losses = dict()
             loss_weights = dict()
 
             cell_mask = tf.math.greater(y[0], 0)
-            cell_mask_interior = tf.math.greater(y[0], 1) if self.gcdm_derivatives else None
+            cell_mask_interior = tf.math.greater(y[0], 1) if self.gcdm_derivatives or self.predict_gcdm_derivatives else None
             # edm
             if edm_weight>0:
-                edm_loss = compute_loss_derivatives(y[0], y_pred[0], self.edm_loss, derivatives=self.edm_derivatives, laplacian=self.edm_derivatives)
+                #edm_loss = self.edm_loss(y[0], edm)
+                edm_loss = compute_loss_derivatives(y[0], edm, self.edm_loss, pred_dy=edm_dy, pred_dx=edm_dx, derivative_loss=self.edm_derivatives, laplacian_loss=self.edm_derivatives)
                 losses["edm"] = edm_loss
                 loss_weights["edm"] = edm_weight
 
             # center
             if center_weight>0:
-                center_loss = compute_loss_derivatives(y[1], y_pred[1], self.gdcm_loss, mask=cell_mask, mask_interior=cell_mask_interior, derivatives=self.gcdm_derivatives)
+                center_loss = compute_loss_derivatives(y[1], gcdm, self.gcdm_loss, pred_dy=gcdm_dy, pred_dx=gcdm_dx, mask=cell_mask, mask_interior=cell_mask_interior, derivative_loss=self.gcdm_derivatives)
                 losses["center"] = center_loss
                 loss_weights["center"] = center_weight
 
@@ -127,7 +139,7 @@ class DiSTNetModel(tf.keras.Model):
             losses["loss"] = loss
 
         if self.print_gradients:
-            trainable_vars_tape = [t for t in self.trainable_variables if (t.name.startswith("DecoderSegEDM") or t.name.startswith("DecoderCenter0") or t.name.startswith("DecoderTrackY0") or t.name.startswith("DecoderTrackX0") or t.name.startswith("DecoderCat0") or t.name.startswith("FeatureSequence/Op4") or t.name.startswith("Attention")) and ("/kernel" in t.name or "/wv" in t.name) ]
+            trainable_vars_tape = [t for t in self.trainable_variables if (t.name.startswith("DecoderSegEDM") or t.name.startswith("DecoderCenterGCDM") or t.name.startswith("DecoderTrackY0") or t.name.startswith("DecoderTrackX0") or t.name.startswith("DecoderCat0") or t.name.startswith("FeatureSequence/Op4") or t.name.startswith("Attention")) and ("/kernel" in t.name or "/wv" in t.name) ]
             for loss_name, loss_value in losses.items():
                 if loss_name != "loss" :
                     w = loss_weights[loss_name] # outside tape: cannot modify loss_value -> need to apply w to gradient itself
@@ -243,6 +255,8 @@ def get_distnet_2d_model(input_shape,  # Y, X
                          next:bool=True,
                          long_term:bool = True,
                          predict_next_displacement:bool = True,
+                         predict_edm_derivatives:bool = False,
+                         predict_gcdm_derivatives:bool = False,
                          l2_reg:float = 0,
                          name: str="DiSTNet2D",
                          **kwargs,
@@ -301,7 +315,13 @@ def get_distnet_2d_model(input_shape,  # Y, X
         get_seq_and_filters = lambda l : [l[i] for i in [0, 3]]
         decoder_feature_op={n: get_seq_and_filters(parse_param_list(feature_decoder_settings, f"Features{n}", l2_reg=l2_reg, last_input_filters=feature_filters)) for n in decoder_layers.keys()}
         decoder_out={"Seg":{}, "Center":{}, "Track":{}, "Cat":{}}
-        output_per_decoder = {"Seg": ["EDM"], "Center": ["Center"], "Track": ["dY", "dX"] if not predict_next_displacement else ["dY", "dX", "dYNext", "dXNext"], "Cat": ["Cat"] if not predict_next_displacement else ["Cat", "CatNext"]}
+        output_per_decoder = {"Seg": ["EDM"], "Center": ["GCDM"], "Track": ["dY", "dX"] if not predict_next_displacement else ["dY", "dX", "dYNext", "dXNext"], "Cat": ["Cat"] if not predict_next_displacement else ["Cat", "CatNext"]}
+        if predict_edm_derivatives:
+            output_per_decoder["Seg"].append("EDMdY")
+            output_per_decoder["Seg"].append("EDMdX")
+        if predict_gcdm_derivatives:
+            output_per_decoder["Center"].append("GCDMdY")
+            output_per_decoder["Center"].append("GCDMdX")
         n_frame_pairs = n_chan -1
         if long_term:
             n_frame_pairs = n_frame_pairs + (frame_window-1) * (2 if next else 1)
@@ -311,8 +331,10 @@ def get_distnet_2d_model(input_shape,  # Y, X
 
         for l_idx, param_list in enumerate(decoder_settings):
             if l_idx==0:
-                decoder_out["Seg"]["EDM"] = decoder_op(**param_list, size_factor=contraction_per_layer[l_idx], mode=upsampling_mode, skip_combine_mode=skip_combine_mode, combine_kernel_size=combine_kernel_size, activation_out="linear", filters_out=1, l2_reg=l2_reg, layer_idx=l_idx, name=f"DecoderSegEDM")
-                decoder_out["Center"]["Center"] = decoder_op(**param_list, size_factor=contraction_per_layer[l_idx], mode=upsampling_mode, skip_combine_mode=skip_combine_mode, combine_kernel_size=combine_kernel_size, activation_out="linear", filters_out=1, l2_reg=l2_reg, layer_idx=l_idx, name=f"DecoderCenter")
+                for dSegName in output_per_decoder["Seg"]:
+                    decoder_out["Seg"][dSegName] = decoder_op(**param_list, size_factor=contraction_per_layer[l_idx], mode=upsampling_mode, skip_combine_mode=skip_combine_mode, combine_kernel_size=combine_kernel_size, activation_out="linear", filters_out=1, l2_reg=l2_reg, layer_idx=l_idx, name=f"DecoderSeg{dSegName}")
+                for dCenterName in output_per_decoder["Center"]:
+                    decoder_out["Center"][dCenterName] = decoder_op(**param_list, size_factor=contraction_per_layer[l_idx], mode=upsampling_mode, skip_combine_mode=skip_combine_mode, combine_kernel_size=combine_kernel_size, activation_out="linear", filters_out=1, l2_reg=l2_reg, layer_idx=l_idx, name=f"DecoderCenter{dCenterName}")
                 for dTrackName in output_per_decoder["Track"]:
                     decoder_out["Track"][dTrackName] = decoder_op(**param_list, size_factor=contraction_per_layer[l_idx], mode=upsampling_mode, skip_combine_mode=skip_combine_mode, combine_kernel_size=combine_kernel_size, activation_out="linear", filters_out=1, l2_reg=l2_reg, layer_idx=l_idx, name=f"DecoderTrack{dTrackName}")
                 for dCatName in output_per_decoder["Cat"]:
@@ -325,7 +347,7 @@ def get_distnet_2d_model(input_shape,  # Y, X
         for n, o_ns in output_per_decoder.items():
             decoder_output_names[n] = dict()
             for o_n in o_ns:
-                decoder_output_names[n][o_n] = f"Output{oi}_{o_n}"
+                decoder_output_names[n][o_n] = f"Output{oi:02}_{o_n}"
                 oi += 1
         # Create GRAPH
         input = tf.keras.layers.Input(shape=spatial_dims+[n_chan], name="Input")
@@ -400,7 +422,7 @@ def get_distnet_2d_model(input_shape,  # Y, X
                     if output_name in decoder_out[decoder_name]:
                         d_out = decoder_out[decoder_name][output_name]
                         layer_output_name = decoder_output_names[decoder_name][output_name]
-                        if not is_segmentation and predict_next_displacement:
+                        if not is_segmentation and predict_next_displacement or decoder_name=="Seg" and predict_edm_derivatives or decoder_name== "Center" and predict_gcdm_derivatives:
                             layer_output_name += "_" # will be concatenated -> output name is used @ concat
                         up_out = d_out([up, residuals[-1] if 0 in skip else None]) # (N_OUT x B, Y, X, F)
                         up_out = BatchToChannel(n_splits = n_chan if is_segmentation else n_frame_pairs, compensate_gradient = False, name = layer_output_name)(up_out)
@@ -410,8 +432,15 @@ def get_distnet_2d_model(input_shape,  # Y, X
                         if k.endswith("Next"):
                             output_name = k.replace("Next", "")
                             output_per_dec[output_name] = tf.keras.layers.Concatenate(axis = -1, name = decoder_output_names[decoder_name][output_name])([output_per_dec[output_name], output_per_dec.pop(k)])
+                if decoder_name=="Seg" and predict_edm_derivatives:
+                    output_name = "EDM"
+                    output_per_dec[output_name] = tf.keras.layers.Concatenate(axis=-1,  name=decoder_output_names[decoder_name][output_name])([output_per_dec[output_name], output_per_dec.pop("EDMdY"), output_per_dec.pop("EDMdX")])
+                if decoder_name=="Center" and predict_gcdm_derivatives:
+                    output_name = "GCDM"
+                    output_per_dec[output_name] = tf.keras.layers.Concatenate(axis=-1,  name=decoder_output_names[decoder_name][output_name])([output_per_dec[output_name], output_per_dec.pop("GCDMdY"), output_per_dec.pop("GCDMdX")])
                 outputs.extend(output_per_dec.values())
-        return DiSTNetModel([input], outputs, name=name, frame_window=frame_window, next = next, spatial_dims=spatial_dims if attention > 0 or self_attention > 0 else None, long_term=long_term, predict_next_displacement=predict_next_displacement, **kwargs)
+        return DiSTNetModel([input], outputs, name=name, frame_window=frame_window, next=next, spatial_dims=spatial_dims if attention > 0 or self_attention > 0 else None, long_term=long_term, predict_next_displacement=predict_next_displacement, predict_gcdm_derivatives=predict_gcdm_derivatives, predict_edm_derivatives=predict_edm_derivatives, **kwargs)
+
 
 def encoder_op(param_list, downsampling_mode, skip_stop_gradient:bool = False, l2_reg:float=0, last_input_filters:int=0, name: str="EncoderLayer", layer_idx:int=1):
     name=f"{name}{layer_idx}"

distnet_2d/utils/image_derivatives_tf.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 try:
     import tensorflow_probability as tfp
@@ -22,21 +23,21 @@ def der_2d(image, axis:int):
         Raises:
         ValueError: If `image` is not a 4D tensor.
         """
-
-    tf.assert_equal(tf.rank(image), 4, message=f'image_gradients expects a 4D tensor  [B, Y, X, C], not {image.get_shape()}.')
+    if isinstance(image, np.ndarray):
+        assert image.ndim == 4, f'image_gradients expects a 4D tensor  [B, Y, X, C], not {image.shape}'
+    else:
+        tf.assert_equal(tf.rank(image), 4, message=f'image_gradients expects a 4D tensor  [B, Y, X, C], not {tf.shape(image)}.')
     assert axis in [1, 2], "axis must be in [1, 2]"
     image_shape = tf.shape(image)
     B, Y, X, C = tf.unstack(image_shape)
     if axis == 1:
-        dy = 0.5 * (image[:, 2:, :, :] - image[:, :-2, :, :])
-        shape = tf.stack([B, 1, X, C])
-        zeros = tf.zeros(shape, image.dtype)
+        dy = tf.math.divide(image[:, 2:, :, :] - image[:, :-2, :, :], tf.cast(2, image.dtype))
+        zeros = tf.zeros(tf.stack([B, 1, X, C]), image.dtype)
         dy = tf.concat([zeros, dy, zeros], 1)
         return tf.reshape(dy, image_shape)
     else:
-        dx = 0.5 * (image[:, :, 2:, :] - image[:, :, :-2, :])
-        shape = tf.stack([B, Y, 1, C])
-        zeros = tf.zeros(shape, image.dtype)
+        dx = tf.math.divide(image[:, :, 2:, :] - image[:, :, :-2, :], tf.cast(2, image.dtype))
+        zeros = tf.zeros(tf.stack([B, Y, 1, C]), image.dtype)
         dx = tf.concat([zeros, dx, zeros], 2)
         return tf.reshape(dx, image_shape)
 

distnet_2d/utils/losses.py
@@ -28,25 +28,40 @@ class PseudoHuber(tf.keras.losses.Loss):
         return tf.multiply(self.delta_sq, tf.sqrt(1. + tf.square((y_true - y_pred)/self.delta)) - 1.)
 
 
-def compute_loss_derivatives(y, y_pred, loss_fun, mask=None, mask_interior=None, derivatives: bool = False, gradient: bool = False, laplacian: bool = False):
-    loss = loss_fun(y, tf.where(mask, y_pred, 0) if mask is not None else y_pred)
-    #print(f"compute loss with mask: {mask is not None} interior: {mask_interior is not None} der: {derivatives} grad: {gradient} lap: {laplacian}", flush=True)
-    if derivatives or gradient or laplacian:
+def compute_loss_derivatives(true, pred, loss_fun, pred_dy=None, pred_dx=None, pred_grad=None, pred_lap=None, mask=None, mask_interior=None, derivative_loss: bool = False, gradient_loss: bool = False, laplacian_loss: bool = False):
+    loss = loss_fun(true, tf.where(mask, pred, 0) if mask is not None else pred)
+    #print(f"compute loss with mask: {mask is not None} interior: {mask_interior is not None} der: {derivative_loss} grad: {gradient_loss} lap: {laplacian_loss} pred lap: {y_pred_lap is not None} pred dy: {y_pred_dy is not None} pred dx: {y_pred_dx is not None}", flush=True)
+    if derivative_loss or gradient_loss or laplacian_loss or pred_dy is not None or pred_dx is not None or pred_grad is not None or pred_lap is not None:
         if mask_interior is None:
             mask_interior = mask
-        dy, dx = der.der_2d(y, 1), der.der_2d(y, 2)
-        dy_pred, dx_pred = der.der_2d(y_pred, 1), der.der_2d(y_pred, 2)
-        if laplacian:
+        dy, dx = der.der_2d(true, 1), der.der_2d(true, 2)
+        if derivative_loss or gradient_loss or laplacian_loss:
+            dy_pred, dx_pred = der.der_2d(pred, 1), der.der_2d(pred, 2)
+        if laplacian_loss or pred_lap is not None:
             lap = der.laplacian_2d(None, dy, dx)
+        if pred_lap is not None:
+            pred_lap = tf.where(mask_interior, pred_lap, 0) if mask_interior is not None else pred_lap
+            loss = loss + loss_fun(lap, pred_lap)
+        if laplacian_loss:
             lap_pred = der.laplacian_2d(None, dy_pred, dx_pred)
             lap_pred = tf.where(mask_interior, lap_pred, 0) if mask_interior is not None else lap_pred
             loss = loss + loss_fun(lap, lap_pred)
-        if gradient:
+        if gradient_loss or pred_grad is not None:
             grad = dy * dy + dx * dx
+        if pred_grad is not None:
+            pred_grad = tf.where(mask_interior, pred_grad, 0) if mask_interior is not None else pred_grad
+            loss = loss + loss_fun(grad, pred_grad)
+        if gradient_loss:
             grad_pred = dy_pred * dy_pred + dx_pred * dx_pred
             grad_pred = tf.where(mask_interior, grad_pred, 0) if mask_interior is not None else grad_pred
             loss = loss + loss_fun(grad, grad_pred)
-        if derivatives:
+        if pred_dy is not None:
+            pred_dy = tf.where(mask_interior, pred_dy, 0) if mask_interior is not None else pred_dy
+            loss = loss + loss_fun(dy, pred_dy)
+        if pred_dx is not None:
+            pred_dx = tf.where(mask_interior, pred_dx, 0) if mask_interior is not None else pred_dx
+            loss = loss + loss_fun(dx, pred_dx)
+        if derivative_loss:
             dy_pred = tf.where(mask_interior, dy_pred, 0) if mask_interior is not None else dy_pred
             dx_pred = tf.where(mask_interior, dx_pred, 0) if mask_interior is not None else dx_pred
             loss = loss + loss_fun(dy, dy_pred) + loss_fun(dx, dx_pred)