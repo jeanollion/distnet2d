distnet2d : added group in dataset scaling computation. removed auto focal weight ScheduledDropout & ScheduledGradientWeight: corrected progress computation. Cat loss: removed temperature

distnet_2d/model/distnet_2d.py
@@ -6,6 +6,7 @@ from pyexpat import features
 
 import tensorflow as tf
 
+from dataset_iterator.keras_layers import Identity
 from .temporal_pyramid import TemporalPyramid, TemporalFeatureReconstructor, TemporalFeaturePairReconstructor, \
     TemporalFeatureReconstructorV6
 from .temporal_cross_attention import TemporalCrossAttention
@@ -17,7 +18,7 @@ from .layers import ker_size_to_string, Combine, ResConv2D, Conv2DBNDrop, Conv2D
     FrameDistanceEmbedding, Conv2DWithDtype, Conv2DTransposeWithDtype, \
     InferenceLayer, InferenceAwareBatchSelector, RelativeTemporalEmbedding, HybridThresholdL2Regularizer, \
     ScheduledDropout, ScheduledGradientWeight, ResidualGradientLimiter, LogGradientMagnitude, \
-    ConcatenateWithDtype, Identity
+    ConcatenateWithDtype
 import numpy as np
 
 from .local_spatial_attention import LocalSpatialAttention
@@ -25,7 +26,7 @@ from .spatial_attention import SpatialAttention2D
 from .temporal_attention import TemporalAttention
 from ..utils.helpers import ensure_multiplicity, flatten_list
 from ..utils.losses import weighted_loss_by_category, balanced_category_loss, PseudoHuber, compute_loss_derivatives, \
-    TemperedFocalCrossEntropy
+    FocalCrossEntropy
 from ..utils.agc import adaptive_clip_grad
 from .gradient_accumulator import GradientAccumulator
 import time
@@ -82,17 +83,17 @@ class DiSTNetModel(tf.keras.Model):
         if link_multiplicity_class_weights is not None:
             assert len(link_multiplicity_class_weights) == 3, "3 link multiplicity class weights should be provided: normal cell, dividing/merging cells, cell with no previous cell"
             self.link_multiplicity_class_weights = HideVariableWrapper( tf.Variable(np.asarray(link_multiplicity_class_weights, dtype="float32"), dtype=tf.float32, trainable=False,  name="link_multiplicity_class_weights"))
-            self.link_multiplicity_loss = weighted_loss_by_category(TemperedFocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE), self.link_multiplicity_class_weights.value, remove_background=True)
+            self.link_multiplicity_loss = weighted_loss_by_category(FocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE), self.link_multiplicity_class_weights.value, remove_background=True)
         else:
-            self.link_multiplicity_loss = balanced_category_loss(TemperedFocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE), 3, max_class_frequency=link_multiplicity_max_class_weight, remove_background=True)
+            self.link_multiplicity_loss = balanced_category_loss(FocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE), 3, max_class_frequency=link_multiplicity_max_class_weight, remove_background=True)
         if category_number > 1:
             if category_class_weights is not None:
                 assert len(category_class_weights) == category_number, f"{category_number} category weights should be provided {len(category_class_weights)} where provided instead ({category_class_weights})"
                 self.category_class_weights = HideVariableWrapper(tf.Variable(np.asarray(category_class_weights, dtype="float32"), dtype=tf.float32, trainable=False, name="category_class_weights"))
-                self.category_loss = weighted_loss_by_category(TemperedFocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE, focal_weight=category_focal_weight), self.category_class_weights.value, remove_background=True)
+                self.category_loss = weighted_loss_by_category(FocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE, focal_weight=category_focal_weight), self.category_class_weights.value, remove_background=True)
             else:
-                self.category_loss = balanced_category_loss(TemperedFocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE), category_number, max_class_frequency=category_max_class_weight, remove_background=True)
-            self.fgbg_category_loss = weighted_loss_by_category(TemperedFocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE, temperature=1, focal_weight=0), [1., 1.], remove_background=False)
+                self.category_loss = balanced_category_loss(FocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE), category_number, max_class_frequency=category_max_class_weight, remove_background=True)
+            self.fgbg_category_loss = weighted_loss_by_category(FocalCrossEntropy(reduction=tf.keras.losses.Reduction.NONE, focal_weight=0), [1., 1.], remove_background=False)
         else:
             self.category_loss = None
 
@@ -863,11 +864,12 @@ def encoder_op(param_list, downsampling_mode, skip_stop_gradient:bool = False, l
             Id = tf.keras.layers.Identity if hasattr(tf.keras.layers, 'Identity') else Identity
             res = Id(name=f"residual{layer_idx}", autocast=False)(x)
             x =Id(autocast=False)(x)  # tf.identity(x) to split path so that input of ResidualGradientLimiter do not contain residual gradient (gradient merging happens before tf.identity)
-            min_progress = 0.25 + (0.8 - 0.25) * (total_layers - 1 - layer_idx - 1) / (total_layers - 1) if layer_idx < total_layers - 1 else 0.
-            max_progress = 0.25 + (0.8 - 0.25) * (total_layers - 1 - layer_idx) / (total_layers - 1) # deepest skip reaches minimal rate before shallowest skip
-            #if layer_idx == 0:
-            #    res = LogGradientMagnitude(name=f"res_weighted{layer_idx}")(res)
-            #res = ScheduledGradientWeight(min_progress=min_progress, max_progress=max_progress, name=f"res_grad_weight{layer_idx}")(res)
+            progress_interval = [0.2, 0.7]
+            min_progress = progress_interval[0] + (progress_interval[1] - progress_interval[0]) * (  total_layers - (layer_idx + 1)) / total_layers
+            max_progress = progress_interval[0] + (progress_interval[1] - progress_interval[0]) * ( total_layers - layer_idx) / total_layers  # deepest skip reaches minimal rate before shallowest skip
+            print(f"layer: {layer_idx + 1}/{total_layers} progress range: [{min_progress}; {max_progress}]")
+
+            res = ScheduledGradientWeight(min_progress=min_progress, max_progress=max_progress, name=f"res_grad_weight{layer_idx}")(res)
             #res = LogGradientMagnitude(name=f"res_limited{layer_idx}")(res)
             res, x = ResidualGradientLimiter(max_ratio=task_with_skip_prop, name=f"res_grad_limiter{layer_idx}")([res, x])
             #x = LogGradientMagnitude(name=f"main{layer_idx}")(x)
@@ -875,7 +877,7 @@ def encoder_op(param_list, downsampling_mode, skip_stop_gradient:bool = False, l
             #    res = LogGradientMagnitude(name=f"res{layer_idx}")(res)
             if skip_stop_gradient:
                 res = stop_gradient(res, parent_name = name)
-            #res = ScheduledDropout(rate=0.0, max_rate=0.9, min_progress=min_progress, max_progress=max_progress, spatial=True, name=f"res_dropout{layer_idx}")(res) # pushes the network to use deepest features
+            res = ScheduledDropout(rate=0.0, max_rate=0.9, min_progress=min_progress, max_progress=max_progress, spatial=True, name=f"res_dropout{layer_idx}")(res) # pushes the network to use deepest features
             #res = LogGradientMagnitude(name=f"res_before_dropout{layer_idx}")(res)
             n_splits, inference_idx = skip_parameters
             assert inference_idx<n_splits, f"invalid inference idx: {inference_idx} must be lower than n_splits: {n_splits}"

distnet_2d/model/layers.py
@@ -1,12 +1,9 @@
 from tensorflow import pad
+
+from dataset_iterator.keras_layers import InferenceLayer
 from ..utils.helpers import ensure_multiplicity
 import tensorflow as tf
 import numpy as np
-
-class InferenceLayer:
-    def __init__(self, *args, **kwargs):
-        self.inference_mode = False
-        super().__init__(*args, **kwargs)
 
 
 class InferenceAwareSelector(InferenceLayer, tf.keras.layers.Layer):
@@ -874,7 +871,7 @@ class ScheduledDropout(tf.keras.layers.Layer):
             return inputs
 
         current_rate = self.get_current_rate()
-
+        current_rate = tf.cast(current_rate, inputs.dtype)
         if self.spatial:
             # Use tf.nn.dropout with spatial noise_shape for spatial dropout
             input_shape = tf.shape(inputs)
@@ -888,17 +885,19 @@ class ScheduledDropout(tf.keras.layers.Layer):
 
             return tf.nn.dropout(
                 inputs,
-                rate=tf.cast(current_rate, inputs.dtype),
+                rate=current_rate,
                 noise_shape=noise_shape,
                 seed=self.seed
-            )
+            ) * (1 - current_rate) # unscaled dropout
+
         else:
             # Regular dropout (no noise_shape = element-wise dropout)
             return tf.nn.dropout(
                 inputs,
-                rate=tf.cast(current_rate, inputs.dtype),
+                rate=current_rate,
                 seed=self.seed
-            )
+            ) * (1 - current_rate)
+
 
     def set_progress(self, progress_value):
         """Set global training progress [0, 1] - called by callback"""
@@ -912,8 +911,8 @@ class ScheduledDropout(tf.keras.layers.Layer):
         # Calculate layer-specific progress
         # Cast to float32 explicitly to handle mixed precision
         progress_val = tf.cast(self.progress, tf.float32) - tf.cast(self.min_progress, tf.float32)
-        max_progress_val = tf.cast(self.max_progress, tf.float32)
-        layer_progress = tf.maximum(0.0, tf.minimum(1.0, progress_val / max_progress_val))
+        progress_norm = tf.cast(self.max_progress - self.min_progress, tf.float32)
+        layer_progress = tf.maximum(0.0, tf.minimum(1.0, progress_val / progress_norm))
 
         # Interpolate from max_rate to min_rate
         max_rate_val = tf.cast(self.max_rate, tf.float32)
@@ -1092,17 +1091,6 @@ class RelativeTemporalEmbedding(tf.keras.layers.Layer):
         else:
             return additive_emb
 
-# for compat with tf2.7
-class Identity(tf.keras.layers.Layer):
-    def __init__(self, autocast=False, **kwargs):
-        super().__init__(autocast=autocast **kwargs)
-
-    def call(self, input, training=None):
-        if not training:
-            return input
-        return tf.identity( input, name=self.name )
-
-
 class ConcatenateWithDtype(InferenceLayer, tf.keras.layers.Concatenate):
     def __init__(self, *args, inference_idx=None, output_dtype:str=None, autocast=False, **kwargs):
         self.output_dtype = output_dtype
@@ -1131,7 +1119,6 @@ class ConcatenateWithDtype(InferenceLayer, tf.keras.layers.Concatenate):
         config = super().get_config()
         config.update({'output_dtype': self.output_dtype, "inference_idx": self.inference_idx})
         return config
-
 
 
 # Gradient manipulation
@@ -1288,8 +1275,8 @@ class ScheduledGradientWeight(tf.keras.layers.Layer):
         # Calculate layer-specific progress
         # Cast to float32 explicitly to handle mixed precision
         progress_val = tf.cast(self.progress, tf.float32) -  tf.cast(self.min_progress, tf.float32)
-        max_progress_val = tf.cast(self.max_progress, tf.float32)
-        layer_progress = tf.maximum(0.0, tf.minimum(1.0, progress_val / max_progress_val))
+        progress_norm = tf.cast(self.max_progress - self.min_progress, tf.float32)
+        layer_progress = tf.maximum(0.0, tf.minimum(1.0, progress_val / progress_norm))
 
         # Interpolate from min_weight to max_weight (inverse of dropout)
         min_weight_val = tf.cast(self.min_weight, tf.float32)

distnet_2d/utils/callbacks.py
@@ -220,7 +220,7 @@ class ScheduledDropoutCallback(tf.keras.callbacks.Callback):
     based on training epochs. Each layer handles its own schedule via max_progress.
     """
 
-    def __init__(self, n_epochs, verbose=1):
+    def __init__(self, n_epochs, verbose=0):
         """
         Args:
             n_epochs: Total number of training epochs
@@ -285,7 +285,7 @@ class ScheduledDropoutCallback(tf.keras.callbacks.Callback):
         # Each layer will compute its own rate based on its max_progress
         for layer in self.dropout_layers:
             layer.set_progress(global_progress)
-            if self.verbose > 0 and epoch % 100  == 0:
+            if self.verbose > 0 and epoch % 50  == 0:
                 print(f"epoch: {epoch} layer: {layer.name} dropout rate: {layer.get_current_rate()}")
 
 
@@ -295,7 +295,7 @@ class ScheduledGradientCallback(tf.keras.callbacks.Callback):
     based on training epochs. Each layer handles its own schedule via max_progress.
     """
 
-    def __init__(self, n_epochs, verbose=1):
+    def __init__(self, n_epochs, verbose=0):
         """
         Args:
             n_epochs: Total number of training epochs
@@ -354,7 +354,7 @@ class ScheduledGradientCallback(tf.keras.callbacks.Callback):
         # Update all layers with the same global progress, each layer will compute its own weight based on its max_progress
         for layer in self.gradient_layers:
             layer.set_progress(global_progress)
-            if self.verbose > 0 and epoch % 100 == 0: # and epoch % 100 == 0
+            if self.verbose > 0 and epoch % 50 == 0: # and epoch % 100 == 0
                 print(f"epoch: {epoch} layer: {layer.name} gradient weight: {layer.get_current_weight()}")
 
 

distnet_2d/utils/losses.py
@@ -28,8 +28,8 @@ class PseudoHuber(tf.keras.losses.Loss):
         return tf.multiply(self.delta_sq, tf.sqrt(1. + tf.square((y_true - y_pred)/self.delta)) - 1.)
 
 
-class TemperedFocalCrossEntropy(tf.keras.losses.Loss):
-    def __init__(self, temperature: float = 2.0, focal_weight = 2.0,
+class FocalCrossEntropy(tf.keras.losses.Loss):
+    def __init__(self, focal_weight = 2.0,
                  label_smoothing: float = 0, **kwargs):
         """
         Tempered Focal Cross-Entropy with Label Smoothing for multi-class classification.
@@ -37,11 +37,6 @@ class TemperedFocalCrossEntropy(tf.keras.losses.Loss):
         and regularization (label smoothing).
 
         Args:
-            temperature: Tempering parameter (t ≥ 1). Controls gradient bounding.
-                        t=1.0 → standard focal loss (unbounded gradients)
-                        t=2.0 → moderate bounding (recommended start)
-                        t=3.0+ → strong bounding (very stable, may slow learning)
-
             focal_weight: Focusing parameter (γ ≥ 0). Controls hard example emphasis. can be a list / tuple -> one value for each class
                    γ=0.0 → tempered CE (no focal effect)
                    γ=1.0 → mild focus on hard examples
@@ -71,14 +66,12 @@ class TemperedFocalCrossEntropy(tf.keras.losses.Loss):
                             - Improves test accuracy & calibration
                             - Can conflict with focal loss (both modify targets)
         """
-        self.temperature = float(temperature)
         if focal_weight is None or (isinstance(focal_weight, (float, int)) and focal_weight == 0):
             self.focal_weight = None
         else:
             self.focal_weight = np.atleast_1d(np.array(focal_weight, dtype=np.float32))
         self.label_smoothing = float(label_smoothing)
 
-        assert temperature >= 1, f"temperature must be >=1, got {temperature}"
         assert focal_weight is None or np.all(focal_weight >= 0), f"gamma must be >=0, got {focal_weight}"
         assert 0 <= label_smoothing < 1, f"label_smoothing must be in [0,1), got {label_smoothing}"
 
@@ -87,8 +80,8 @@ class TemperedFocalCrossEntropy(tf.keras.losses.Loss):
     def call(self, y_true, y_pred):
         """
         Args:
-            y_true: One-hot encoded labels, shape (batch_size, num_classes)
-            y_pred: Predicted probabilities, shape (batch_size, num_classes)
+            y_true: One-hot encoded labels, shape (batch_size, (Y, X), num_classes)
+            y_pred: Predicted probabilities, shape (batch_size, (Y, X), num_classes)
         """
         epsilon = tf.keras.backend.epsilon()
         y_pred = tf.clip_by_value(y_pred, epsilon, 1. - epsilon)
@@ -97,12 +90,6 @@ class TemperedFocalCrossEntropy(tf.keras.losses.Loss):
         if self.label_smoothing > 0:
             num_classes = tf.cast(tf.shape(y_true)[-1], y_true.dtype)
             y_true = y_true * (1. - self.label_smoothing) + self.label_smoothing / num_classes
-
-        # Tempered log: (p^(1-t) - 1) / (1-t)
-        if self.temperature > 1:
-            tempered_log = (tf.pow(y_pred, 1. - self.temperature) - 1.) / (1. - self.temperature)
-        else:
-            tempered_log = tf.math.log(y_pred)
 
         # Focal weight: (1 - p)^gamma
         # Note: With label smoothing, focal effect is slightly reduced
@@ -118,9 +105,8 @@ class TemperedFocalCrossEntropy(tf.keras.losses.Loss):
             focal_weight = tf.cast(1, y_true.dtype)
 
         # Combined loss
-        loss = - focal_weight * y_true * tempered_log
-
-        return tf.reduce_sum(loss, axis=-1)
+        loss = - focal_weight * y_true * tf.math.log(y_pred)
+        return loss
 
     def get_config(self):
         config = super().get_config()
@@ -222,53 +208,3 @@ def balanced_category_loss(original_loss_func, n_classes, max_class_frequency=10
             class_weights = sample_weight * class_weights
         return original_loss_func(y_true, y_pred, sample_weight=class_weights)
     return loss_func
-
-
-def compute_focal_weights(class_counts,
-                          base_value=1.0,
-                          range=(0.25, 10.0),
-                          power_law = 0.4):
-    """
-    Compute per-class focal weight values based on class frequencies.
-
-    INTUITION:
-    - Rare classes need higher gamma (more aggressive focusing)
-    - Common classes need lower gamma (less aggressive, avoid noise)
-
-    Args:
-        class_counts: Array of sample counts per class, shape (num_classes,)
-        base_value: Baseline gamma value (typically 2.0)
-        range: (min_gamma, max_gamma) to clip results
-        power_law: float
-            power_law 0.2 (conservative) to 0.4 (aggressive)
-
-    Returns:
-        per_class_gamma: Array of shape (num_classes,)
-
-    STRATEGY DETAILS:
-       gamma_i = base_gamma × (max_freq / freq_i)^α
-
-       Example with α=0.25: [10000, 1000, 100, 10] samples
-       → gamma ≈ [2.0, 2.4, 2.8, 3.6]
-
-       Properties:
-       - α controls aggressiveness (0.2-0.3 typical)
-       - More aggressive than log for extreme imbalance
-       - Risk of overfitting rare classes if α too high
-
-    """
-    class_counts = np.array(class_counts, dtype=np.float32)
-    class_counts = np.maximum(class_counts, 1.0)
-    mean_freq = class_counts.mean()
-    min_gamma, max_gamma = range
-
-    # gamma_i = base × (max_freq / freq_i)^α
-    # Rationale: Power law for more aggressive rare class boosting
-    freq_ratios = mean_freq / class_counts
-    gamma_multipliers = np.power(freq_ratios, power_law)
-    per_class_gamma = base_value * gamma_multipliers
-
-    # Clip to reasonable range
-    per_class_gamma = np.clip(per_class_gamma, min_gamma, max_gamma)
-
-    return per_class_gamma